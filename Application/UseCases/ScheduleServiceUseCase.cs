33c3ab9534f6248650417db8a231c9357ff6a7e4
DanielVieiraFernandes <danivieiiira@gmail.com>
2025-09-21 01:48:50 -0300

    feat: adicionando caso de uso de agendar um serviço na barbearia

---
+++ Application/UseCases/ScheduleServiceUseCase.cs
@@ -0,0 +1,37 @@
+﻿using GestaoDeBarbearia.Communication.Requests;
+using GestaoDeBarbearia.Communication.Responses;
+using GestaoDeBarbearia.Domain.Entities;
+using GestaoDeBarbearia.Domain.Repositories;
+
+namespace GestaoDeBarbearia.Application.UseCases;
+public class ScheduleServiceUseCase
+{
+    private IScheduleRepository scheduleRepository;
+    public ScheduleServiceUseCase(IScheduleRepository scheduleRepository)
+    {
+        this.scheduleRepository = scheduleRepository;
+    }
+
+    public async Task<ResponseScheduledServiceJson> Execute(RequestScheduleServiceJson request)
+    {
+        Appointment appointment = new()
+        {
+            AppointmentDateTime = request.AppointmentDateTime,
+            ClientId = request.ClientId,
+            ClientName = request.ClientName,
+            ClientPhone = request.ClientPhone,
+            EmployeeId = request.EmployeeId,
+            ServicePrice = request.ServicePrice,
+            PaymentType = request.PaymentType
+        };
+
+        await scheduleRepository.Create(appointment);
+
+        return new ResponseScheduledServiceJson
+        {
+            AppointmentDateTime = request.AppointmentDateTime,
+            ServicePrice = request.ServicePrice,
+            EmployeeId = request.EmployeeId
+        };
+    }
+}
