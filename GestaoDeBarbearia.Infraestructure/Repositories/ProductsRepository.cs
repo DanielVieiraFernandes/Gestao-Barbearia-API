431bc403c0fe5da47aec2e04643963de1cf384f4
DanielVieiraFernandes <danivieiiira@gmail.com>
2025-10-02 22:03:50 -0300

    feat(infra): implementa repositrios de products, sales e schedules

---
+++ GestaoDeBarbearia.Infraestructure/Repositories/ProductsRepository.cs
@@ -0,0 +1,68 @@
+﻿using Dapper;
+using GestaoDeBarbearia.Domain.Entities;
+using GestaoDeBarbearia.Domain.Enums;
+using GestaoDeBarbearia.Domain.Pagination.Products;
+using GestaoDeBarbearia.Domain.Repositories;
+using GestaoDeBarbearia.Infraestructure.Utils;
+using Npgsql;
+using System.Text;
+
+namespace GestaoDeBarbearia.Infraestructure.Repositories;
+public class ProductsRepository : IProductsRepository
+{
+
+    private DBFunctions dbFunctions;
+
+    public ProductsRepository(DBFunctions dbFunctions)
+    {
+        this.dbFunctions = dbFunctions;
+    }
+
+    public async Task Create(Product product)
+    {
+        await using NpgsqlConnection connection = await dbFunctions.CreateNewConnection();
+
+        string sql = DBFunctions.CreateInsertQuery<Product>("barber_shop_products");
+
+        await connection.ExecuteAsync(sql, product);
+    }
+
+    public async Task<Product?> FindById(long id)
+    {
+        await using NpgsqlConnection connection = await dbFunctions.CreateNewConnection();
+
+        string sql = "SELECT * FROM barber_shop_products WHERE id = @Id;";
+
+        return await connection.QueryFirstOrDefaultAsync<Product>(sql, new { Id = id });
+    }
+
+    public async Task<List<Product>> GetAll(RequestProductsPaginationParamsJson? paginationParams = null)
+    {
+        await using NpgsqlConnection connection = await dbFunctions.CreateNewConnection();
+
+        RequestProductsPaginationParamsJson paginationParamsJson = paginationParams ?? new();
+
+        StringBuilder sql = new();
+
+        sql.Append("SELECT * FROM barber_shop_products ");
+        sql.Append($@"ORDER BY {paginationParamsJson.OrderByColumn.ToString().ToLower()} 
+            {paginationParamsJson.OrderByDirection.GetEnumDescription()} ");
+
+        var result = await connection.QueryAsync<Product>(sql.ToString(), paginationParams);
+
+        if (result is null || !result.Any())
+            return [];
+
+        // Equivalente a result.ToList()
+        return [.. result];
+    }
+
+    public async Task Update(Product product)
+    {
+        await using NpgsqlConnection connection = await dbFunctions.CreateNewConnection();
+
+        string updateQuery = DBFunctions.CreateUpdateQuery<Product>("barber_shop_products");
+
+        await connection.ExecuteAsync(updateQuery, product);
+    }
+}
---
+++ GestaoDeBarbearia.Infraestructure/Repositories/SalesRepository.cs
@@ -0,0 +1,114 @@
+﻿using Dapper;
+using GestaoDeBarbearia.Domain.Entities;
+using GestaoDeBarbearia.Domain.Pagination.Sales;
+using GestaoDeBarbearia.Domain.Repositories;
+using GestaoDeBarbearia.Exception.ExceptionsBase;
+using GestaoDeBarbearia.Infraestructure.Utils;
+using System.Text;
+
+namespace GestaoDeBarbearia.Infraestructure.Repositories;
+public class SalesRepository : ISalesRepository
+{
+    private readonly DBFunctions dbFunctions;
+    private readonly IProductsRepository productsRepository;
+    public SalesRepository(DBFunctions dbFunctions, IProductsRepository productsRepository)
+    {
+        this.dbFunctions = dbFunctions;
+        this.productsRepository = productsRepository;
+    }
+    public async Task<(Sale, List<SaleDetails>)> Create(Sale sale, List<SaleDetails> saleDetails)
+    {
+        await using var connection = await dbFunctions.CreateNewConnection();
+
+        List<Product> productsToUpdate = [];
+
+        foreach (var sd in saleDetails)
+        {
+            var product = await productsRepository.FindById(sd.ProductId);
+
+            if (product is null)
+                throw new NotFoundException($"Produto não encontrado");
+
+            if (product.Quantity < sd.Quantity)
+                throw new ErrorRegisteringSaleException($"Estoque insuficiente para o produto {product.Name}");
+
+            if (product.Quantity - sd.Quantity < product.MinimumStock)
+                throw new ErrorRegisteringSaleException($"Estoque do produto {product.Name} está abaixo do estoque mínimo.");
+
+            product.Quantity -= sd.Quantity;
+
+            productsToUpdate.Add(product);
+
+            sd.UnitSalePrice = product.SalePrice * sd.Quantity;
+
+            sale.SaleTotal += sd.UnitSalePrice;
+        }
+
+        using var transaction = await connection.BeginTransactionAsync();
+
+        StringBuilder sql = new();
+
+        sql.Append(DBFunctions.CreateInsertQuery<Sale>("barber_shop_sales"));
+        sql.Append(" RETURNING *; ");
+
+        var createdSale = await connection.QuerySingleAsync<Sale>(sql.ToString(), sale);
+
+        if (createdSale is null)
+        {
+            await transaction.RollbackAsync();
+            throw new ErrorRegisteringSaleException("Erro ao registrar venda");
+        }
+
+        sql.Clear();
+
+        saleDetails.ForEach(sd => sd.SaleId = createdSale.Id);
+
+        sql.Append(DBFunctions.CreateInsertQuery<SaleDetails>("barber_shop_sale_details"));
+        var linesCreatedSaleDetails = await connection.ExecuteAsync(sql.ToString(), saleDetails);
+
+        if (linesCreatedSaleDetails != saleDetails.Count)
+        {
+            await transaction.RollbackAsync();
+            throw new ErrorRegisteringSaleException("Erro ao registrar detalhes da venda");
+        }
+
+        var createdSaleDetails = await connection.QueryAsync<SaleDetails>("SELECT * FROM barber_shop_sale_details WHERE saleid = @SaleId;", new { SaleId = createdSale.Id });
+
+        if (createdSaleDetails is null || !createdSaleDetails.Any())
+        {
+            await transaction.RollbackAsync();
+            throw new ErrorRegisteringSaleException("Erro ao registrar detalhes da venda");
+        }
+
+        sql.Clear();
+
+        sql.Append(DBFunctions.CreateUpdateQuery<Product>("barber_shop_products"));
+
+        var linesUpdatedProducts = await connection.ExecuteAsync(sql.ToString(), productsToUpdate);
+
+        if (linesUpdatedProducts != productsToUpdate.Count)
+        {
+            await transaction.RollbackAsync();
+            throw new ErrorRegisteringSaleException("Erro ao atualizar estoque dos produtos");
+        }
+
+        await transaction.CommitAsync();
+
+        return (createdSale, [.. createdSaleDetails]);
+    }
+
+    public Task<Sale> FindById(long id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<SaleDetails>> FindDetailsById(long saleId)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<Sale>> GetAll(RequestSalesPaginationParamsJson? paginationParams = null)
+    {
+        throw new NotImplementedException();
+    }
+}
