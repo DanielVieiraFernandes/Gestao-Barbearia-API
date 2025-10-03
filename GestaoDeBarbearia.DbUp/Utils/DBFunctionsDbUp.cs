96b719e034add584f6ad653724c8761bbcb2a09b
DanielVieiraFernandes <danivieiiira@gmail.com>
2025-10-02 22:03:49 -0300

    chore(dbup): cria funões utilitrias para manipulaão de banco via DbUp

---
+++ GestaoDeBarbearia.DbUp/Utils/DBFunctionsDbUp.cs
@@ -0,0 +1,49 @@
+﻿using System.Reflection;
+
+namespace GestaoDeBarbearia.DbUp.Utils;
+public class DBFunctionsDbUp
+{
+    public static string CreateInsertQuery<T>(string tableName)
+    {
+        Type type = typeof(T);
+        PropertyInfo[] props = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+
+        List<string> columns = [];
+        List<string> parameters = [];
+
+        // Propriedades a serem excluídas da query
+        List<string> excludeProps = ["Id", "CreatedAt", "UpdatedAt"];
+
+        foreach (var prop in props)
+        {
+            // Ignorar propriedades que estão na lista de exclusão
+            if (excludeProps.Contains(prop.Name))
+                continue;
+
+            columns.Add(prop.Name.ToLower());
+            parameters.Add($"@{prop.Name}");
+        }
+
+        string insertSQL = $"INSERT INTO {tableName} ({string.Join(", ", columns)}) VALUES ({string.Join(", ", parameters)})";
+
+        return insertSQL;
+    }
+
+    public static string CreateUpdateQuery<T>(string tableName)
+    {
+        Type type = typeof(T);
+        PropertyInfo[] props = type.GetProperties(BindingFlags.Public | BindingFlags.Instance);
+        List<string> setClauses = [];
+        // Propriedades a serem excluídas da query
+        List<string> excludeProps = ["Id", "CreatedAt", "UpdatedAt"];
+        foreach (var prop in props)
+        {
+            // Ignorar propriedades que estão na lista de exclusão
+            if (excludeProps.Contains(prop.Name))
+                continue;
+            setClauses.Add($"{prop.Name.ToLower()} = @{prop.Name}");
+        }
+        string updateSQL = $"UPDATE {tableName} SET {string.Join(", ", setClauses)} WHERE id = @Id;";
+        return updateSQL;
+    }
+}
