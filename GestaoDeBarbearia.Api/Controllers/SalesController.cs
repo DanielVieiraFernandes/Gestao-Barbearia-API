9e437a8011df802076c096b74f2af848662e0c50
DanielVieiraFernandes <danivieiiira@gmail.com>
2025-10-02 22:03:48 -0300

    feat(api): adiciona SalesController para gerenciar vendas

---
+++ GestaoDeBarbearia.Api/Controllers/SalesController.cs
@@ -0,0 +1,24 @@
+﻿using GestaoDeBarbearia.Application.UseCases.Sales;
+using GestaoDeBarbearia.Communication.Requests;
+using GestaoDeBarbearia.Communication.Responses;
+using Microsoft.AspNetCore.Mvc;
+
+namespace GestaoDeBarbearia.Api.Controllers;
+[Route("api/[controller]")]
+[ApiController]
+public class SalesController : ControllerBase
+{
+    /// <summary>
+    /// Método para registrar uma nova venda.
+    /// </summary>
+    /// <param name="request"></param>
+    /// <param name="useCase"></param>
+    /// <returns></returns>
+    [HttpPost]
+    [ProducesResponseType<ResponseSaleJson>(StatusCodes.Status201Created)]
+    public async Task<IActionResult> RegisterSale([FromBody] RequestRegisterSaleJson request, [FromServices] RegisterSaleUseCase useCase)
+    {
+        var result = await useCase.Execute(request);
+        return Created(string.Empty, result);
+    }
+}
