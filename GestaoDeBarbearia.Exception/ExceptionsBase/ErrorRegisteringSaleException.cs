787906edd26aeaa73336e34044fffcb1e6cb1031
DanielVieiraFernandes <danivieiiira@gmail.com>
2025-10-02 22:03:50 -0300

    feat(exception): adiciona exceão especfica para falha no registro de venda

---
+++ GestaoDeBarbearia.Exception/ExceptionsBase/ErrorRegisteringSaleException.cs
@@ -0,0 +1,18 @@
+﻿
+using System.Net;
+
+namespace GestaoDeBarbearia.Exception.ExceptionsBase;
+public class ErrorRegisteringSaleException : GestaoDeBarbeariaException
+{
+    public override int StatusCode => (int)HttpStatusCode.BadRequest;
+
+    public ErrorRegisteringSaleException(string message) : base(message)
+    {
+
+    }
+
+    public override List<string> GetErrors()
+    {
+        return [Message];
+    }
+}
